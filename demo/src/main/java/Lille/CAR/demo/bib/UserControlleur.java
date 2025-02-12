6c687664b5e19c05affac7f0b955843f51232504
sepideh94 <sepidesoleymani922@gmail.com>
2025-02-12 11:36:41 +0100

    iteration 1, second commit

--- demo/src/main/java/Lille/CAR/demo/bib/UserControlleur.java
+++ demo/src/main/java/Lille/CAR/demo/bib/UserControlleur.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/store")
@@ -18,7 +19,7 @@ public class UserControlleur {
     public ModelAndView home(@RequestParam(value = "error", required = false) String error) {
         ModelAndView modelAndView = new ModelAndView("home");
         if (error != null) {
-            modelAndView.addObject("error", "Email ou mot de passe incorrect !");
+            modelAndView.addObject("error", "Email ou mot de passe incorrect!");
         }
         return modelAndView;
     }
@@ -31,17 +32,25 @@ public class UserControlleur {
         User newUser = new User(email, password, firstName, lastName);
         service.register(newUser);
         return new RedirectView("/store/home");
+        
     }
 
     @PostMapping("/login")
-    public RedirectView login(@RequestParam String email, @RequestParam String password) {
+    public RedirectView login(@RequestParam String email, @RequestParam String password, HttpSession session) {
         User user = service.login(email, password);
         if (user != null) {
+            session.setAttribute("user", user); 
             return new RedirectView("/store/commande");
         }
         return new RedirectView("/store/home?error=true");
     }
-
+    
+    @GetMapping("/logout")
+    public RedirectView logout(HttpSession session) {
+        session.invalidate(); 
+        return new RedirectView("/store/home"); 
+    }
+    
     @GetMapping("/commande")
     public ModelAndView commandPage() {
         return new ModelAndView("/commande");
